90e05484d931bea644809ce938c7fd19a6011e82
Alexander De Simone <cristianoali08i@gmail.com>
2022-07-12 16:51:25 -0300

    agrego temas

--- js/app.js
+++ js/app.js
@@ -1,12 +1,108 @@
 'use strict';
 
+const menu = [
+  { nombre: 'Pizza Muzzarella x6', precio: 900 },
+  { nombre: 'Pizza  Muzzarella x10', precio: 1550 },
+  { nombre: 'Pizza Especial x6', precio: 1370 },
+  { nombre: 'Pizza Especial x10', precio: 2100 },
+  { nombre: 'Papas Fritas', precio: 950 },
+  { nombre: 'Gaseosa 500 ml', precio: 250 },
+  { nombre: 'Cerveza', precio: 500 },
+];
+
+for (let i = 0; i < menu.length; i++) {
+  console.log(`${menu[i].nombre} - Precio: ${menu[i].precio}`);
+}
+for (let producto of menu) {
+  console.log(producto);
+  console.log(producto.precio);
+  console.log(producto.nombre);
+}
+
+menu.forEach(function (producto) {
+  console.log(`${producto.nombre} - Precio: ${producto.precio}`);
+});
+
+const arr1 = menu.map(function (producto) {
+  console.log(`${producto.nombre} - Precio: ${producto.precio}`);
+  return `${producto.nombre} - Precio: ${producto.precio}`;
+});
+console.log(arr1);
+
+//--------------------------------------------------------------------------------------------
+
+const existe = menu.some(producto => producto.nombre === 'Papas Fritas');
+console.log(existe);
+
+if (existe === true) {
+  console.log('Sale Con Fritas🍟😝');
+}
+
+let total = 0;
+function agregarPlato(precio) {
+  return (total += precio);
+}
+//propina del 10%
+function calcularPropina(total) {
+  return total * 0.1;
+}
+function totalConPropina(total) {
+  return total * 1.1;
+}
+console.table(menu);
+
+total = agregarPlato(300);
+total = agregarPlato(400);
+total = agregarPlato(500);
+
+const totalPagar = totalConPropina(total);
+const propina = calcularPropina(total);
+console.log(`El consumo total fue :  💲${total}`);
+console.log(`propina  10 %  💲${propina}`);
+console.log(`El total a pagar es de: 💲${totalPagar}`);
+//--------------------------------------------------------------------------------------------
+console.log(
+  '----------------------------------------------------------------------------'
+);
+
+//--------------------------------------------------------------------------------------------
+let total2 = 0;
+const mesa = [
+  { nombre: 'Pizza Muzzarella x6', precio: 900 },
+  { nombre: 'Papas  Fritas', precio: 950 },
+  { nombre: 'Cerveza', precio: 500 },
+];
+
+mesa.forEach(producto => (total2 += producto.precio));
+console.log(`El consumo total fue :  💲${total2}`);
+
+/* 
+
+//funcionalidades iniciales/requerimientos iniciales
+
+validación de campos ventana modal
+Menu:  AGREGAR/MOSTRAR/ELIMINAR  
+
+Resumen de consumo  MOSTRAR DATOS MODAL(Mesa/hora) / CONSUMO / ELIMINAR
+
+Propina:  CALCULAR  5% 10% 25%
+
+//mostrar SubTotal /  Propina  / Total a Pagar
+*/
+
+//crear una api o bd falsa para testeo y consulta.
+
 //variables
+/*
 
 let cliente = {
   mesa: ' ',
   hora: ' ',
   pedido: [],
 };
+
+
+//MODAL  CARGAR MESA
 
 // seleccionar el documento   ⚠
 const btnGuardarCliente = '#guardar-cliente';
@@ -23,6 +119,8 @@ function guardarCliente() {
   const mesa = '#mesa';
   const hora = '#hora';
   //console.log('probando función');
+ 
+ 
   //revisar campos vacíos
   const camposVacios = [mesa, hora].some(campo => campo === ' ');
 
@@ -34,10 +132,22 @@ function guardarCliente() {
   }
 
   //ASIGNAR LOS DATOS  DEL FORM A CLIENTE
-  //spread operator "cliente" para mantener el arreglo pedidos [ ] al ingresar la mesa
+  //spread operator  "cliente" 1° | para mantener el arreglo pedidos [ ] al ingresar la mesa y la hora
   cliente = { ...cliente, mesa, hora };
 
   console.log(cliente);
 
   //Ocultar Modal
+
+  //obtener menu de una bd o API.
+  obtenerMenu();
+
+  //Mostrar las secciones
+  function mostrarSecciones() {
+    //remover d-none cuando la mesa pasa la validación
+
+    //const seccionesOcultas = ('.d-none');
+    seccionesOcultas.forEach(seccion => seccion.classList.remove('d-none'));
+  }
 }
+*/
