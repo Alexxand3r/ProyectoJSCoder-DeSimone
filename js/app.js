614039a64958b059f02db0f32a433747079be655
Alexander De Simone <cristianoali08i@gmail.com>
2022-07-19 10:57:04 -0300

    correccion en la maquetacion,reserva mesa y notas,responsive

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 'use strict';
 
-//Calculadora
-
-//variables
+// DARK MODE
 
 const darkMode = document.getElementById('darkMode');
 
@@ -20,6 +18,90 @@ function cambiarModo() {
   document.getElementsByClassName('table')[0].classList.toggle('dark');
 }
 
+/*
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+// AGREGAR NOTAS
+
+//variables
+const formularioNotas = document.querySelector('#formulario-notas');
+const listaNotas = document.querySelector('#lista-notas');
+
+let notas = [];
+
+//eventos
+escucharEvento();
+function escucharEvento() {
+  formularioNotas.addEventListener('submit', agregarNota);
+}
+
+//Funciones
+
+function agregarNota(e) {
+  e.preventDefault();
+  // console.log('probando');
+
+  //textarea donde el usuario guarda la mesa.
+  const nota = document.querySelector('#notas').value;
+  //validación
+  if (nota === '') {
+    console.log('probando vació');
+    mostrarError('No ingresaste contenido! ⚠');
+    return; //evita q se ejecuten mas linea de código
+  }
+  //console.log('probando');
+  //asignar un id con (Date.now) no hay base de datos aun..
+  const notaObj = {
+    id: Date.now(),
+    nota,
+  };
+
+  //añadir al arreglo notas [ ]
+  notas = [...notas, notaObj];
+  console.log(notas);
+  //crear HTML
+  crearHTML();
+  //reiniciar el formulario
+  formularioNotas.reset();
+}
+
+//mostrar mensaje de error
+function mostrarError(error) {
+  const mensajeError = document.createElement('p');
+  mensajeError.textContent = error;
+  mensajeError.classList.add('error');
+  //insertar en el contenido
+  const contenido = document.querySelector('#contenido');
+  contenido.appendChild(mensajeError);
+  //elimina el mensaje después de 2.5seg
+  setTimeout(() => {
+    mensajeError.remove();
+  }, 2500);
+}
+
+//Crear HTML
+function crearHTML() {
+  limpiarHTML();
+
+  if (notas.length > 0) {
+    notas.forEach(nota => {
+      //Crear HTML
+      const li = document.createElement('li');
+      //Añadir texto
+      li.innerText = nota.nota;
+      //insertarlo en el html
+      listaNotas.appendChild(li);
+    });
+  }
+}
+
+//limpiar el HTML
+function limpiarHTML() {
+  while (listaNotas.firstChild) {
+    listaNotas.removeChild(listaNotas.firstChild);
+  }
+}
 /*
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 */
