7b791ea637f9d73c01130f6ba2a62e30beca6a1b
Alexander De Simone <cristianoali08i@gmail.com>
2022-08-09 23:44:01 -0300

    correciones finales/ingreso traductor

---
+++ js/languages.js
@@ -0,0 +1,14 @@
+const languages = [
+  { 'en-GB': 'English' },
+  { 'es-ES': 'Spanish' },
+  { 'fr-FR': 'French' },
+  { 'it-IT': 'Italian' },
+  { 'ja-JP': 'Japanese' },
+  { 'ko-KR': 'Korean' },
+  { 'la-VA': 'Latin' },
+  { 'nl-NL': 'Dutch' },
+  { 'pt-PT': 'Portuguese' },
+  { 'ru-RU': 'Russian' },
+];
+
+export default languages;
---
+++ js/traductor.js
@@ -0,0 +1,64 @@
+'use strict';
+
+import languages from '../js/languages.js';
+
+const selectFirst = document.querySelector('.first');
+const selectSecond = document.querySelector('.second');
+const translate = document.querySelector('.translate');
+const fromText = document.querySelector('.fromText');
+const toText = document.querySelector('.toText');
+const change = document.getElementById('change');
+const reades = document.querySelectorAll('.read');
+
+const language1 = 'en-GB';
+const language2 = 'es-ES';
+
+//BOTON CAMBIAR
+change.addEventListener('click', _ => {
+  const selectFirstValue = selectFirst.value;
+  selectFirst.value = selectSecond.value;
+  selectSecond.value = selectFirstValue;
+
+  if (!toText.value) return;
+  const fromTextValue = fromText.value;
+  fromText.value = toText.value;
+  toText.value = fromTextValue;
+});
+
+for (const i in languages) {
+  const key = Object.keys(languages[i]).toString();
+  const value = Object.values(languages[i]).toString();
+
+  // console.log(`${key} = ${value}`);
+
+  selectFirst.innerHTML += `<option value=${key}>${value}</option>`;
+  selectSecond.innerHTML += `<option value=${key}>${value}</option>`;
+}
+
+selectFirst.value = language1;
+selectSecond.value = language2;
+
+//https://mymemory.translated.net/doc/spec.php
+
+translate.addEventListener('click', async _ => {
+  //si el texto esta vació,retorna y no se ejecuta las funciones q siguen
+  if (!fromText.value) return;
+
+  const res = await fetch(
+    `https://api.mymemory.translated.net/get?q=${fromText.value}&langpair=${selectFirst.value}|${selectSecond.value}`
+  );
+  //convertir la respuesta en json
+  const data = await res.json();
+  //console.log(data.responseData.translatedText);
+  toText.value = data.responseData.translatedText;
+});
+
+reades.forEach((read, index) => {
+  read.addEventListener('click', _ => {
+    const textToRead = index == 0 ? fromText.value : toText.value;
+    //console.log(textToRead);
+    //si el texto es nulo retorne
+    if (!textToRead) return;
+    speechSynthesis.speak(new SpeechSynthesisUtterance(textToRead));
+  });
+});
